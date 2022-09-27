944778178611028b14909fda4a5993eb5030f268
Klemen Pavlovič <klemenpavlovic@gmail.com>
2022-09-27 11:05:19 +0200

    Add some functionality to react-datetime component

--- pages/react-datetime.js
+++ pages/react-datetime.js
@@ -5,8 +5,28 @@ import styles from "../styles/Datetime.module.css";
 import "react-datetime/css/react-datetime.css";
 
 import Datetime from "react-datetime";
+import moment from "moment";
+// import "moment/locale/fr";
+import "moment/locale/sl";
 
 export default function ReactDatetime() {
+  // Below we have all the props that we can use with the <DateTime> component. There are also some methods that can be used imperatively.
+  // value => Represents the selected date by the component, in order to use it as a controlled component. This prop is parsed by Moment.js, so it is possible to use a date string or a moment object.
+  // input => Whether to show an input field to edit the date manually.
+  // open => Whether to open or close the picker. If not set react-datetime will open the datepicker on input focus and close it on click outside.
+  // locale => Manually set the locale for the react-datetime instance. Moment.js locale needs to be loaded to be used, see i18n docs.
+  // onChange => Callback trigger when the date changes. The callback receives the selected moment object as only parameter, if the date in the input is valid. If the date in the input is not valid, the callback receives the value of the input (a string).
+  // onOpen, onClose => Callback trigger for when the user opens/closes the datepicker.
+  // isValidDate => Define the dates that can be selected. The function receives (currentDate, selectedDate) and shall return a true or false whether the currentDate is valid or not. See selectable dates.
+
+  const yesterday = moment().subtract(1, "day");
+  const valid = (current) => {
+    // return current.isAfter(yesterday);
+    return (
+      current.day() !== 0 && current.day() !== 6 && current.isAfter(yesterday)
+    );
+  };
+
   return (
     <div>
       <nav style={{ display: "flex", gap: "10px", margin: "20px 10px" }}>
@@ -21,7 +41,13 @@ export default function ReactDatetime() {
         </Link>
       </nav>
       <div className={styles.center}>
-        <Datetime />
+        <Datetime
+          input={true}
+          open
+          locale="sl"
+          onChange={(date) => console.log("Date changed! ", date._d)}
+          isValidDate={valid}
+        />
       </div>
     </div>
   );
